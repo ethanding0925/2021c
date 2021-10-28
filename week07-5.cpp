0af01193f7431c40d702d9394b5eac6698f5bd98
ethanding <91250389+ethanding0925@users.noreply.github.com>
2021-10-28 17:45:00 +0800

    Add files via upload

---
+++ week07-5.cpp
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+    for (int i=0;i<3;i++){printf("筿福癹伴程糶猭:\n",i);}
+        for (int i=1;i<=3;i++) {printf("摸计计,眖1计:%d\n",i);}
+            for (int i=0;i<=3;i++){printf("ぃ糶猭:\n",i);}
+}
